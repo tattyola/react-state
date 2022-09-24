ebbc05e71b92e64098927a788c56618ea3ad6074
Tatsiana A <astrouskaya@yahoo.com>
2022-09-24 19:30:31 -0400

    created calculator

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import {useState} from "react";
 
 function App() {
 
-    const [name, setName] = useState('react lesson');
-    const changeName = (e) => {
-        // console.log(e.target.value)
-        setName(e.target.value)
+    const [number1, setNumber1] = useState(10);
+    const [number2, setNumber2] = useState(10);
+    const changeNumber1 = (e) => {
+        isNaN(e.target.value) ? setNumber1(0) : setNumber1(+e.target.value);
+
+    }
+    const changeNumber2 = (e) => {
+        isNaN(e.target.value) ? setNumber2(0) : setNumber2(+e.target.value);
     }
 
     return (
         <div>
-            <input value={name} onChange={changeName}/>
+            <input value={number1} onChange={changeNumber1}/>
+            <input value={number2} onChange={changeNumber2}/>
+            <h1>{number1} + {number2} = {number1 + number2}</h1>
         </div>
     );
 }
