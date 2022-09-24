2cc124b5dd407a7c3ff599e70d88872640b307e4
Tatsiana A <astrouskaya@yahoo.com>
2022-09-24 19:12:57 -0400

    added useState for text input

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import {useState} from "react";
 
 function App() {
 
-  const [name, setName] = useState('react lesson');
-  const changeName = (e) => {
-    // console.log(e.target.value)
-    setName(e.target.value)
-  }
+    const [name, setName] = useState('react lesson');
+    const changeName = (e) => {
+        // console.log(e.target.value)
+        setName(e.target.value)
+    }
 
-  return (
-    <div>
-      <input value={name} onChange={changeName}/>
-    </div>
-  );
+    return (
+        <div>
+            <input value={name} onChange={changeName}/>
+        </div>
+    );
 }
 
 export default App;
